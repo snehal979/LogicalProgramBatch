c53daa107b118c2361fd993eaedcd053c6ceef8a
Snehal979 <bansodsnehal535@gmail.com>
2023-01-30 21:12:20 +0530

    [Snehal]Add: fibonacci Series for Nth Number

---
+++ LogicalProgramBatch/FibonacciSeries.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramBatch
+{
+    internal class FibonacciSeries
+    {
+        public void CalculatedFibonacciSeries()
+        {
+            /* Fibonacci Series
+         0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 14
+         fib1,fib2,fib3,fib4,_________
+         Fn = Fn-1 + Fn-2*/ // F1 & F2 is 0 & 1 resp
+                            //F3 = F2+F1;
+            //variable fib1 =0 and fib 2 = 1 (always start)
+            int fib1 = 0, fib2 = 1;
+            int fib3;
+            int temp;
+            Console.WriteLine("Enter the nth number Upto nth fibonacciSeries");
+            int numberRange = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("FibonacciSeries");
+            Console.WriteLine(fib1); // for position 1
+            Console.WriteLine(fib2); // for position 2
+            for(int i = 3; i <= numberRange; i++)  //start position by 3
+            {
+                fib3 =fib2+fib1;
+               // Fn = Fn-1 + Fn-2
+                Console.WriteLine(fib3);
+                fib1 = fib2;  
+                fib2 = fib3;  
+            }
+
+        }
+    }
+}
--- LogicalProgramBatch/Program.cs
+++ LogicalProgramBatch/Program.cs
@@ -5,6 +5,30 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Logical Programs");
+            bool flag = true;
+            while (flag)
+            {
+                try
+                {
+                    Console.WriteLine("Hint 1 FibonacciSeries 2.Exit");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            FibonacciSeries series = new FibonacciSeries();
+                            series.CalculatedFibonacciSeries();
+                            break;
+                        case 2:
+                            flag = false;
+                            Console.WriteLine("Exit");
+                            break;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
